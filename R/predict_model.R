3be8e742a0951180225c79d4271afb95203aaa4d
Robert Ramos <rjramos116@gmail.com>
2018-04-23 10:37:54 -0500

    Initial import of predict.R

---
+++ R/predict_model.R
@@ -0,0 +1,10 @@
+# 1) Takes the GLM model and raster stack and creates an output raster
+#
+# Args
+# input_predictors             A raster stack
+# input_model                  A GLM model object
+#
+# Output
+# pb                     A raster of predicted liklehood of occurance
+predict_model <- function(input_predictors, input_model){
+}
